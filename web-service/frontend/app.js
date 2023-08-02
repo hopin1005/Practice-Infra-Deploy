d13757ad28ef7e10fb83fb4808d55eacfb91cd16
hopin1005 <asd89541321@gmail.com>
2023-08-02 09:38:57 +0800

    add new path to backend

--- web-service/frontend/app.js
+++ web-service/frontend/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const axios = require('axios'); // 新增這行
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('hi this is frontend website!!');
+});
+
+app.get('/backend', (req, res) => {
+
+  axios.get('http://backend-service-internal-ip/backend')
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+      res.send('Error while connecting to backend service.');
+    });
 });
 
 app.listen(port, () => {
